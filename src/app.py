caa37654446484eff781c78ab8a293d6d45e9661
Mathieu Bekkaye <bekkaye.m@gmail.com>
2024-03-07 06:05:03 +0100

    3. Adding Chat functionnality

--- src/app.py
+++ src/app.py
@@ -16,40 +16,64 @@
 
 
 
-import streamlit as st                                              #1
-from langchain_core.messages import HumanMessage, AIMessage         #2
-from dotenv import load_dotenv                                      #1
+import streamlit as st                                                      
+from langchain_core.messages import HumanMessage, AIMessage                 
+from langchain_core.prompts import ChatPromptTemplate                       
+from langchain_core.output_parsers import StrOutputParser                   
+from langchain_openai import ChatOpenAI                                     
+from dotenv import load_dotenv                                              
 
-load_dotenv()                                                       #1
+load_dotenv()                                                               
 
 # Initialize chat history variable
-if "chat_history" not in st.session_state:                          #2
-    st.session_state.chat_history = []                              #2
+if "chat_history" not in st.session_state:                                  
+    st.session_state.chat_history = []                                      
 
-# App config                                                        #1
-st.set_page_config(page_title="Streaming bot", page_icon="🤖")      #1
-st.title("Streaming bot")                                           #1
+# App config                                                                
+st.set_page_config(page_title="Streaming bot", page_icon="🤖")              
+st.title("Streaming bot")  
+
+# Get response  
+def get_response(query, chat_history):                                      
+    template = """                                                  
+    You are a helpful assistant.
+    Answer the following questions considering
+    the history of the conversation:
+
+    Chat history: {chat_history}
+    User question: {user_question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)                     
+
+    llm = ChatOpenAI()                                                      
+        
+    chain = prompt | llm | StrOutputParser()                                
+    
+    return chain.invoke({                                                   
+        "chat_history": chat_history,                                       
+        "user_question": user_query,                                        
+    })                                                                      
 
 
 # Conversation
-for message in st.session_state.chat_history:                       #2
-    if isinstance(message, HumanMessage):                           #2
-        with st.chat_message("Human"):                              #2
-            st.markdown(message.content)                            #2
-    else:                                                           #2
-        with st.chat_message("AI"):                                 #2
-            st.markdown(message.content)                            #2
+for message in st.session_state.chat_history:                               
+    if isinstance(message, HumanMessage):                                   
+        with st.chat_message("Human"):                                      
+            st.markdown(message.content)                                    
+    else:                                                                   
+        with st.chat_message("AI"):                                         
+            st.markdown(message.content)                                    
 
-# User Input
-user_query = st.chat_input("Your message")                          #2
-if user_query is not None and user_query != "":                     #2
-    st.session_state.chat_history.append(HumanMessage(user_query))  #2 
+# User input
+user_query = st.chat_input("Your message")                                  
+if user_query is not None and user_query != "":                             
+    st.session_state.chat_history.append(HumanMessage(user_query))           
     
-    with st.chat_message("Human"):                                  #2
-        st.markdown(user_query)                                     #2
+    with st.chat_message("Human"):                                          
+        st.markdown(user_query)                                             
         
-    with st.chat_message("AI"):                                     #2
-        ai_response = "I don't know"                                #2
-        st.markdown(ai_response)                                    #2
+    with st.chat_message("AI"):                                             
+        ai_response = get_response(user_query, st.session_state.chat_history)       
+        st.markdown(ai_response)                                            
     
-    st.session_state.chat_history.append(AIMessage(ai_response))    #2
+    st.session_state.chat_history.append(AIMessage(ai_response))            
